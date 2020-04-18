fa6e0bfd992d9999f7595ff4f402420530976ea5
Dzeru <diana.mashkina.00@gmail.com>
2020-04-18 15:35:22 +0400

    session parser test td iterations

--- src/main/java/com/scribassu/tracto/service/SessionParserImpl.java
+++ src/main/java/com/scribassu/tracto/service/SessionParserImpl.java
@@ -1,5 +1,6 @@
 package com.scribassu.tracto.service;
 
+import com.scribassu.tracto.domain.ExamPeriodEvent;
 import com.scribassu.tracto.entity.ScheduleParserStatus;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,6 +16,23 @@ public class SessionParserImpl implements ScheduleParser {
         Document document = Jsoup.parse(schedule);
         Element sessionTable = document.getElementById("session");
         Elements trs = sessionTable.child(0).children();
+        ExamPeriodEvent examPeriodEvent;
+        for(Element tr : trs) {
+            Elements tds = tr.children();
+            if(tds.size() == 4) {
+                examPeriodEvent = new ExamPeriodEvent();
+                examPeriodEvent.setSubjectName(tds.get(3).text());
+                System.out.println("SESSION ITEM BEGINS");
+                for(Element td : tds) {
+                    System.out.println("td4:" + td.text());
+                }
+            }
+            if(tds.size() == 2) {
+                for(Element td : tds) {
+                    System.out.println("td2:" + td.text());
+                }
+            }
+        }
 
         return null;
     }

0e3a95e159b90b951e6f0282d7d728c2ac66fbfd
Dzeru <diana.mashkina.00@gmail.com>
2020-08-20 14:38:58 +0400

    fix exam period parser

--- src/main/java/com/scribassu/tracto/service/parser/ExamPeriodScheduleParserImpl.java
+++ src/main/java/com/scribassu/tracto/service/parser/ExamPeriodScheduleParserImpl.java
@@ -3,6 +3,7 @@ package com.scribassu.tracto.service.parser;
 import com.scribassu.tracto.domain.*;
 import com.scribassu.tracto.entity.ScheduleParserStatus;
 import com.scribassu.tracto.repository.*;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ExamPeriodScheduleParserImpl implements ScheduleParser {
 
@@ -48,106 +50,113 @@ public class ExamPeriodScheduleParserImpl implements ScheduleParser {
 
     @Override
     public ScheduleParserStatus parseSchedule(String schedule, String departmentURL) {
+        ScheduleParserStatus status = new ScheduleParserStatus();
         Document document = Jsoup.parse(schedule);
         Elements title = document.getElementsByClass(PAGE_TITLE_CLASS);
         Element pageTitle = title.get(0);
         StudentGroup studentGroup = getStudentGroupByPageTitleText(pageTitle.text(), departmentURL);
         if(null == studentGroup) {
-            System.out.println("AAAAAAAAAA");
+            log.error("Fail to find some student group for " + departmentURL);
+            status.setStatus("fail no group");
+            status.setSchedule("s-unknown" + "-" + departmentURL);
         }
-        ScheduleParserStatus status = new ScheduleParserStatus();
-        try {
-            Element sessionTable = document.getElementById(SESSION_ID);
-            Elements trs = sessionTable.child(0).children();
-            examPeriodEventRepository.deleteByStudentGroup(studentGroup);
-            ExamPeriodEvent examPeriodEvent = null;
-            boolean firstTd2 = true;
-            int lastCorrectDay = -1;
-            ExamPeriodMonth lastCorrectExamPeriodMonth = examPeriodMonthRepository.findByNumber(0).orElseThrow(IllegalArgumentException::new);
-            String lastCorrectYear = " ";
-            int lastCorrectHour = 0;
-            int lastCorrectMinute = 0;
-            for(Element tr : trs) {
-                Elements tds = tr.children();
-                if(tds.size() == 4) {
-                    examPeriodEvent = new ExamPeriodEvent();
-                    examPeriodEvent.setStudentGroup(studentGroup);
-                    for(int cell = 0; cell < tds.size(); cell++) {
-                        switch(cell) {
-                            case 0:
-                                String[] date = tds.get(cell).text().split(" ");
-                                try {
-                                    int day = Integer.parseInt(date[0]);
-                                    ExamPeriodMonth examPeriodMonth = examPeriodMonthRepository.findByRusGenitive(date[1]).get(0);
-                                    String year = date[2];
-                                    examPeriodEvent.setDay(day);
-                                    examPeriodEvent.setMonth(examPeriodMonth);
-                                    examPeriodEvent.setYear(year);
-                                    lastCorrectDay = day;
-                                    lastCorrectExamPeriodMonth = examPeriodMonth;
-                                    lastCorrectYear = year;
-                                }
-                                catch(Exception e) {
-                                    examPeriodEvent.setDay(lastCorrectDay);
-                                    examPeriodEvent.setMonth(lastCorrectExamPeriodMonth);
-                                    examPeriodEvent.setYear(lastCorrectYear);
-                                }
-                                break;
-                            case 1:
-                                try {
-                                    String[] time = tds.get(cell).text().split(":");
-                                    int hour = Integer.parseInt(time[0]);
-                                    int minute = Integer.parseInt(time[1]);
-                                    examPeriodEvent.setHour(hour);
-                                    examPeriodEvent.setMinute(minute);
-                                    lastCorrectHour = hour;
-                                    lastCorrectMinute = minute;
-                                }
-                                catch(Exception e) {
-                                    examPeriodEvent.setHour(lastCorrectHour);
-                                    examPeriodEvent.setMinute(lastCorrectMinute);
-                                }
-                                break;
-                            case 2:
-                                examPeriodEvent.setExamPeriodEventType(getExamPeriodEventType(tds.get(cell).text()));
-                                break;
-                            case 3:
-                                examPeriodEvent.setSubjectName(tds.get(cell).text());
-                                break;
+        else {
+            try {
+                Element sessionTable = document.getElementById(SESSION_ID);
+                Elements trs = sessionTable.child(0).children();
+                examPeriodEventRepository.deleteByStudentGroup(studentGroup);
+                ExamPeriodEvent examPeriodEvent = null;
+                boolean firstTd2 = true;
+                int lastCorrectDay = -1;
+                ExamPeriodMonth lastCorrectExamPeriodMonth = examPeriodMonthRepository.findByNumber(0).orElseThrow(IllegalArgumentException::new);
+                String lastCorrectYear = " ";
+                int lastCorrectHour = 0;
+                int lastCorrectMinute = 0;
+                for(Element tr : trs) {
+                    Elements tds = tr.children();
+                    if(tds.size() == 4) {
+                        examPeriodEvent = new ExamPeriodEvent();
+                        examPeriodEvent.setStudentGroup(studentGroup);
+                        for(int cell = 0; cell < tds.size(); cell++) {
+                            switch(cell) {
+                                case 0:
+                                    String[] date = tds.get(cell).text().split(" ");
+                                    try {
+                                        if(date[0].startsWith("0")) {
+                                            date[0] = date[0].substring(1);
+                                        }
+                                        int day = Integer.parseInt(date[0]);
+                                        ExamPeriodMonth examPeriodMonth = examPeriodMonthRepository.findByRusGenitive(date[1]).get(0);
+                                        String year = date[2];
+                                        examPeriodEvent.setDay(day);
+                                        examPeriodEvent.setMonth(examPeriodMonth);
+                                        examPeriodEvent.setYear(year);
+                                        lastCorrectDay = day;
+                                        lastCorrectExamPeriodMonth = examPeriodMonth;
+                                        lastCorrectYear = year;
+                                    }
+                                    catch(Exception e) {
+                                        examPeriodEvent.setDay(lastCorrectDay);
+                                        examPeriodEvent.setMonth(lastCorrectExamPeriodMonth);
+                                        examPeriodEvent.setYear(lastCorrectYear);
+                                    }
+                                    break;
+                                case 1:
+                                    try {
+                                        String[] time = tds.get(cell).text().split(":");
+                                        int hour = Integer.parseInt(time[0]);
+                                        int minute = Integer.parseInt(time[1]);
+                                        examPeriodEvent.setHour(hour);
+                                        examPeriodEvent.setMinute(minute);
+                                        lastCorrectHour = hour;
+                                        lastCorrectMinute = minute;
+                                    }
+                                    catch(Exception e) {
+                                        examPeriodEvent.setHour(lastCorrectHour);
+                                        examPeriodEvent.setMinute(lastCorrectMinute);
+                                    }
+                                    break;
+                                case 2:
+                                    examPeriodEvent.setExamPeriodEventType(getExamPeriodEventType(tds.get(cell).text()));
+                                    break;
+                                case 3:
+                                    examPeriodEvent.setSubjectName(tds.get(cell).text());
+                                    break;
+                            }
                         }
                     }
-                }
-                if(tds.size() == 2) {
-                    if(firstTd2) {
-                        Teacher teacher = getTeacher(tds.get(1).text());
-                        examPeriodEvent.setTeacher(teacher);
-                        firstTd2 = false;
-                    }
-                    else {
-                        examPeriodEvent.setPlace(tds.get(1).text());
-                        firstTd2 = true;
-                        examPeriodEvent = examPeriodEventRepository.save(examPeriodEvent);
-                        if(null != examPeriodEvent.getId()) {
-                            status.setStatus("ok");
+                    if(tds.size() == 2) {
+                        if(firstTd2) {
+                            Teacher teacher = getTeacher(tds.get(1).text());
+                            examPeriodEvent.setTeacher(teacher);
+                            firstTd2 = false;
                         }
                         else {
-                            status.setStatus("fail");
+                            examPeriodEvent.setPlace(tds.get(1).text());
+                            firstTd2 = true;
+                            examPeriodEvent = examPeriodEventRepository.save(examPeriodEvent);
+                            if(null != examPeriodEvent.getId()) {
+                                status.setStatus("ok");
+                            }
+                            else {
+                                status.setStatus("fail to save");
+                            }
                         }
                     }
                 }
             }
+            catch(Exception e) {
+                status.setStatus("fail " + e);
+            }
+            status.setSchedule("s-" + studentGroup.getGroupNumber() + "-" + departmentURL);
         }
-        catch(Exception e) {
-            status.setStatus("fail");
-        }
-        status.setSchedule("s-" + studentGroup.getGroupNumber() + "-" + departmentURL);
         status = scheduleParserStatusRepository.save(status);
 
         return status;
     }
 
     private StudentGroup getStudentGroupByPageTitleText(String text, String departmentURL) {
-        EducationForm educationForm = null;
+        EducationForm educationForm = EducationForm.DO;
         text = text.replace(" группа", "");
         String[] educationForms = {"Дневное", "Заочное", "Вечернее"};
         if(text.startsWith(educationForms[0])) {

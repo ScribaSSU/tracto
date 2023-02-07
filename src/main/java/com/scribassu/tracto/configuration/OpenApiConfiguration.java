3ceaf86094bb931426a0833e6d6d7dd09d2d5159
Dzeru <diana.mashkina.00@gmail.com>
2023-02-07 11:51:35 +0400

    update OpenApi for HTTPS

--- src/main/java/com/scribassu/tracto/configuration/OpenApiConfiguration.java
+++ src/main/java/com/scribassu/tracto/configuration/OpenApiConfiguration.java
@@ -4,9 +4,13 @@ import com.scribassu.tracto.properties.ProjectProperties;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
 @Configuration
 public class OpenApiConfiguration {
 
@@ -20,7 +24,8 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI api() {
         return new OpenAPI()
-                .info(apiInfo());
+                .info(apiInfo())
+                .servers(Collections.singletonList(new Server().url("/api")));
     }
 
     private Info apiInfo() {
